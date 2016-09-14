20e167e1bf0b125e8f2a5062b033f2d3419d3710
Jweers <jonweers@yahoo.com>
2016-09-14 15:58:07 -0600

    Refactored docs and added new resident brochure
    Also removed a bunch of legacy styling

--- docs.php
+++ docs.php
@@ -18,8 +18,35 @@
       <li><i class="fa fa-file-pdf-o"></i> <a href="files/rules-and-regulations.pdf" target="_blank">Rules and Regs</a></li>
       <li>More coming soon..</li>
     </ul>
+    
+    <div class="row card-board">
+      
+      <div class="col-md-3 white-card">
+        <a href="files/rules-and-regulations.pdf" target="_blank">
+          <h4>Rules and Regulations<i class="fa fa-file-pdf-o pull-right"></i></h4>
+          <p class="small">The latest rules and regulations for Swallow Hill Condominium residents.</p>
+          <span class="small label label-default">pdf</span>
+          
+          <i class="fa fa-download file-download-icon" title="download"></i>
+        </a>
+      </div>
+      
+      <div class="col-md-3 white-card">
+        <a href="files/new-resident-brochure.pdf" target="_blank">
+          <h4>New Residents' Brochure<i class="fa fa-file-pdf-o pull-right"></i></h4>
+          <p class="small">A tri-fold brochure filled with valuable information for new owners.</p>
+          <span class="small label label-default">pdf</span>
+          <span class="sr-only">click to download</span>
+          <i class="fa fa-download file-download-icon" title="download"></i>
+        </a>
+      </div>
+    
+    </div>
   
   </main>
+
+    
+
 
   <?php
   //Render the Footer
