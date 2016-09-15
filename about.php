81e2a3cffd48d50f7336b56a54cc844bf1327bce
Jweers <jonweers@yahoo.com>
2016-09-14 23:16:27 -0600

    Updated about text, removed placeholders

--- about.php
+++ about.php
@@ -16,8 +16,7 @@
     <a href="home" class="close"><span aria-hidden="true">&times;</span></a>
     
     <h1>About the HOA</h1>
-    <p>The Swallow Hill Condominiums Home Owner Association ...</p>
-    <p>The Swallow Hill HOA board consists of 5 members, each serving 2 year terms.  Terms of service are offset so that no more than three board members are up for re-election in any given year.  This provides continuity from year to year and helps to ensure unbroken support for the Swallow Hill condominium community. </p>
+    <p>The Swallow Hill Condominium Association, Inc. was incorporated in 2002 and is governed by a board that consists of 5 members, each serving 2 year terms.  The terms of service are offset so that no more than three board members are up for re-election in any given year.  This provides continuity from year to year and helps to ensure unbroken support for the Swallow Hill condominium community.</p>
     
     <div class="row">
       <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 about-block">
